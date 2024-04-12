7b86c1ef3bc5d566f2be1bd3b8ae5982b89312c6
Exar2004 <141692448+Exar2004@users.noreply.github.com>
2024-04-12 23:13:30 +0000

    bom dia

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, image } from 'react-native';
 import { usastate } from 'react';
-import * ImgePicker from 'expo-image-picker';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
   const {image, setimage} = usastate(null);
 
   const carregarFotos = async () => {
-    let resulet = await ImgePicker.lauchImageLibraryAsync({})
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
   }
 
   return (
     <View style={styles.container}>
       <Button title='select foto' onPress={()=>{carregarFotos}}>Selecione uma foto</Button>
-      {image && <image }
-
+      {image && <Image source={{ uri: image }} style={styles.image} />}
       <StatusBar style="auto" />
     </View>
   );
@@ -26,6 +30,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
   },
 
 });
